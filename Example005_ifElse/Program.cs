138d100e7650ddd753f9e45d13054ec173ab2041
Viktorya <vika.kokorina.933@gmail.com>
2023-08-31 15:41:54 +0300

    Example005_ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "вика")
+{
+    Console.WriteLine("Ура,это же Вика!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
